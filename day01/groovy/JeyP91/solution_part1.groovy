a1e7334c44c5027005f8361d5ec9eed7a1290678
BuildTools <unconfigured@null.spigotmc.org>
2019-12-19 15:01:10 +0100

    Update day01

--- day01/groovy/JeyP91/solution_part1.groovy
+++ day01/groovy/JeyP91/solution_part1.groovy
@@ -1,17 +1,27 @@
 #!/usr/bin/env groovy
 
-int fuel = 0
+test()
 
-// iterate over all modules
-new File('input.txt').eachLine('UTF-8') { line ->
-    int mass = Integer.parseInt(line)
-    fuel += calcFuel(mass)
+println calcFuel(new File('input.txt'))
+
+static int calcFuel(File masses) {
+    int fuel = 0
+    
+    // iterate over all modules
+    masses.eachLine('UTF-8') { line ->
+        int mass = Integer.parseInt(line)
+        fuel += calcFuelForSingleMass(mass)
+    }
+    return fuel
 }
 
 // tag::calcFuel[]
-int calcFuel(int mass) {
+static int calcFuelForSingleMass(int mass) {
     return (mass / 3).trunc() - 2
 }
 // end::calcFuel[]
 
-println fuel
+static void test() {
+    assert calcFuelForSingleMass(1969) == 654
+    assert calcFuelForSingleMass(100756) == 33583
+}
