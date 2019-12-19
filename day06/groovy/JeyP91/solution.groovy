b413eb5324e021bdfe1aafabfe0a72adea7797a9
BuildTools <unconfigured@null.spigotmc.org>
2019-12-19 15:01:27 +0100

    Solution day06 part2

--- day06/groovy/JeyP91/solution.groovy
+++ day06/groovy/JeyP91/solution.groovy
@@ -1,31 +1,37 @@
 #!/usr/bin/env groovy
 
 // Read input
-// ArrayList<String> orbitsAsString = Arrays.asList(new File('input.txt').text.split("\\r?\\n"))
-ArrayList<String> orbitsAsString = Arrays.asList(new File('input_test.txt').text.split("\\r?\\n"))
+ArrayList<String> orbitsAsString = Arrays.asList(new File('input.txt').text.split("\\r?\\n"))
+
+test()
 
 OrbitSystem orbSys = new OrbitSystem(orbitsAsString)
 
 println "Solution Part 1: " + orbSys.getTotalOrbitsInSystem()
+println "Solution Part 2: " + orbSys.getOrbitalTransfersBetween("YOU", "SAN")
 
 class OrbitSystem {
     ArrayList<String> orbitsAsString
-    List<OrbitPair> orbitsAsOrbitPairs = new ArrayList<String>()
+    List<OrbitPair> orbitsAsOrbitPairs = new ArrayList<OrbitPair>()
 
-    public OrbitSystem(ArrayList<String> orbitsAsString) {
+    OrbitSystem(ArrayList<String> orbitsAsString) {
         this.orbitsAsString = orbitsAsString
         orbitsAsString.each { orbit ->
             orbitsAsOrbitPairs.add(new OrbitPair(orbit))
         }
     }
 
-    String getCentralOfOuter(String outer) {
+    OrbitPair getOrbitPairOfOuter(String outer) {
         for(int i = 0; i < orbitsAsOrbitPairs.size(); i++) {
             if(orbitsAsOrbitPairs[i].getOuter() == outer) {
-                return orbitsAsOrbitPairs[i].getCentral()
+                return orbitsAsOrbitPairs[i]
             }
         }
         return null
+    }
+
+    String getCentralOfOuter(String outer) {
+        getOrbitPairOfOuter(outer).getCentral()
     }
 
     int getDistanceBetweenCentralAndOuter(String central, String outer) {
@@ -34,9 +40,6 @@ class OrbitSystem {
         while(nextJump != central) {
             nextJump = getCentralOfOuter(nextJump)
             jumps++
-            // println central + ", " + nextJump + ", " + outer
-            // println jumps
-            // println ""
         }
         return jumps
     }
@@ -48,24 +51,81 @@ class OrbitSystem {
         }
         return total
     }
+
+    int getOrbitalTransfersBetween(String start, String finish) {
+        Integer jumps = null
+        List<OrbitPair> pathStartCentral = getPathFromObjectToCentral(start)
+        List<OrbitPair> pathFinishCentral = getPathFromObjectToCentral(finish)
+        if(pathStartCentral[0].getCentral() == finish) {
+            return 0
+        }
+        for(int i = 0; i < pathStartCentral.size(); i++) {
+            if(pathStartCentral[i].getCentral() == finish) {
+                return i
+            }
+            for(int j = 0; j < pathFinishCentral.size(); j++) {
+                if(pathFinishCentral[j].getCentral() == start) {
+                    return j
+                }
+                if(pathStartCentral[i].getCentral() == pathFinishCentral[j].getCentral()) {
+                    return i + j
+                }
+            }
+        }
+        return jumps
+    }
+
+    ArrayList<OrbitPair> getPathFromObjectToCentral(String object) {
+        List<OrbitPair> path = new ArrayList<OrbitPair>()
+        OrbitPair orbit = getOrbitPairOfOuter(object)
+        path.add(orbit)
+        while (orbit.getCentral() != "COM") {
+            orbit = getOrbitPairOfOuter(orbit.getCentral())
+            path.add(orbit)
+        }
+        return path
+    }
+
+    OrbitPair findOrbit
 }
 
 class OrbitPair {
     String central
     String outer
-    public OrbitPair(String orbitAsString) {
-        int seperator = orbitAsString.indexOf(")")
-        this.central = orbitAsString.substring(0, seperator)
-        this.outer = orbitAsString.substring(seperator + 1, orbitAsString.length())
+    OrbitPair(String orbitAsString) {
+        int separator = orbitAsString.indexOf(")")
+        this.central = orbitAsString.substring(0, separator)
+        this.outer = orbitAsString.substring(separator + 1, orbitAsString.length())
     }
-    public OrbitPair(String central, String outer) {
+    OrbitPair(String central, String outer) {
         this.central = central
         this.outer = outer
     }
-    public String getCentral() {
+    String getCentral() {
         return this.central
     }
-    public String getOuter() {
+    String getOuter() {
         return this.outer
     }
+}
+
+static void test() {
+    ArrayList<String> orbitsAsString
+    OrbitSystem orbSys
+
+    // Test part1 example with result 42
+    orbitsAsString = Arrays.asList(new File('input_test1.txt').text.split("\\r?\\n"))
+    orbSys = new OrbitSystem(orbitsAsString)
+    assert orbSys.getCentralOfOuter("J") == "E"
+    assert orbSys.getDistanceBetweenCentralAndOuter("COM", "H") == 3
+    assert orbSys.getTotalOrbitsInSystem() == 42
+
+    // Test part2 example with result 4
+    orbitsAsString = Arrays.asList(new File('input_test2.txt').text.split("\\r?\\n"))
+    orbSys = new OrbitSystem(orbitsAsString)
+    assert orbSys.getPathFromObjectToCentral("YOU").size() == 7
+    assert orbSys.getPathFromObjectToCentral("SAN").size() == 5
+    assert orbSys.getOrbitalTransfersBetween("YOU", "J") == 1
+    assert orbSys.getOrbitalTransfersBetween("YOU", "K") == 0
+    assert orbSys.getOrbitalTransfersBetween("YOU", "SAN") == 4
 }
